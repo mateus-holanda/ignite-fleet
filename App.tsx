c5382228f793bac59615a987c6dd58dd1f900505
Mateus Holanda <mateus23ita@gmail.com>
2023-05-16 19:51:35 -0300

    Integrate RealmDB to the SignIn page to authenticate user

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import { StatusBar } from 'react-native';
+import { AppProvider, UserProvider } from '@realm/react';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { REALM_APP_ID } from '@env';
+
 import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
 
 import theme from './src/theme';
 
@@ -17,13 +21,18 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
+    
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,65 @@
-import { Container, Title, Slogan } from './styles';
-
-import backgroundImg from '../../assets/background.png';
+import { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+import { Realm, useApp } from '@realm/react';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
 
 import { Button } from '../../components/Button';
 
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env';
+
+import backgroundImg from '../../assets/background.png';
+
+import { Container, Title, Slogan } from './styles';
+
+WebBrowser.maybeCompleteAuthSession();
+
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const [_, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ['profile', 'email']
+  });
+
+  const app = useApp();
+
+  async function handleGoogleSignIn() {
+    setIsAuthenticating(true);
+
+    await googleSignIn().then((response) => {
+      if (response.type !== "success") {
+        setIsAuthenticating(false);
+      }
+    });
+  }
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      if (response.authentication?.idToken) {
+        const credentials = Realm.Credentials.jwt(response.authentication.idToken);
+
+        app.logIn(credentials).catch((error) => {
+          console.log(error);
+          Alert.alert('Sign In', 'There was an error trying to authenticate with your Google account.');
+        });
+      } else {
+        Alert.alert('Sign In', 'There was an error trying to authenticate with your Google account.');
+        setIsAuthenticating(false);
+      }
+    }
+  }, [response]);
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
       <Slogan>Managing your vehicles</Slogan>
-      <Button title="SignIn with Google" />
+      <Button
+        title="SignIn with Google"
+        isLoading={isAuthenticating}
+        onPress={handleGoogleSignIn}
+      />
     </Container>
   );
 }
