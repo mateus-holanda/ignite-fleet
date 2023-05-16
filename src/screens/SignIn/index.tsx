c569adf0faa898943a822514f5bced9135e46dcc
Mateus Holanda <mateus23ita@gmail.com>
2023-05-16 14:12:31 -0300

    First commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+export default function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
+
