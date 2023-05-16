7077eff96b1362d76cf9746683c30296af63e35f
Mateus Holanda <mateus23ita@gmail.com>
2023-05-16 20:17:02 -0300

    Implement Home screen with header and LogOut

---
+++ src/components/HomeHeader/index.tsx
@@ -0,0 +1,38 @@
+import { TouchableOpacity } from 'react-native';
+import { Power } from 'phosphor-react-native';
+import { useApp, useUser } from '@realm/react';
+
+import { Container, Greeting, Message, Name, Picture } from './styles';
+
+import theme from '../../theme';
+
+export function HomeHeader() {
+  const app = useApp();
+  const user = useUser();
+
+  function handleLogout() {
+    app.currentUser?.logOut();
+  }
+
+  return (
+    <Container>
+      <Picture
+        source={{ uri: user?.profile.pictureUrl }} 
+        placeholder="L184i9offQof00ayfQay~qj[fQj["
+      />
+      <Greeting>
+        <Message>
+          Hi
+        </Message>
+
+        <Name>
+          {user?.profile.name}
+        </Name>
+      </Greeting>
+
+      <TouchableOpacity activeOpacity={0.7} onPress={handleLogout}>
+        <Power size={32} color={theme.COLORS.GRAY_400} />
+      </TouchableOpacity>
+    </Container>
+  );
+}
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components/native';
 
+import theme from '../../theme';
+
 export const Container = styled.View`
   flex: 1;
+  background: ${theme.COLORS.GRAY_800};
 `;
